facf98d383ac856550663e077b191a1ab30ba1ee
francescofalcone05 <ciccio_97@live.it>
2024-07-01 12:42:18 +0300

    passo un array non associativo in web.php tramite data

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $data = config("store");
+    $data = [
+        "list" =>  config("store")
+    ];
 
     return view('home', $data);
 });
 
 Route::get('/home', function () {
 
-    $data = config("store");
+    $data = [
+        "list" =>  config("store")
+    ];
 
     return view('home', $data);
 });
 
 Route::get('/list', function () {
 
-    $data = config("store");
+    $data = [
+        "list" =>  config("store")
+    ];
 
     return view('card', $data);
 });
